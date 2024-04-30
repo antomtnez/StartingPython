36065bec021f992d6d9f0d202a0c0471ba0a6325
Anto Martínez <antomartinezdev@gmail.com>
2024-04-30 15:30:10 +0200

    Use of break and continue in for loop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-string = "Hola"
+for number in range(6):
+    if number%2 == 0:
+        print(f"Value: {number}")
 
-for char in string:
-    print(char)
-else:
-    print("Fin de ciclo")
+for number in range(6):
+    if number%2 != 0:
+        continue    
+    print(f"Value: {number}")
