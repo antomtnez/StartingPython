b1d1c137c506c75cb59350f9403d48eee26dd60f
Anto Martínez <antomartinezdev@gmail.com>
2024-04-30 13:10:36 +0200

    Working on operators

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-dayCalification = int(input("Califica tu día de hoy(1 - 10): "))
-print("Tu día de hoy estuvo de", dayCalification)
-
-bookTitle = input("Escribe un titulo de un libro: ")
-bookAuthor = input("Escribe el autor de un libro: ")
-print("EL titulo del libro es:", bookTitle)
-print("EL titulo del libro es:", bookAuthor)
-print(bookTitle, "fue escrito por", bookAuthor)
+num1 = 5
+num2 = 3
+result = num1 + num2
+print(f"Result: {result}")
+result = num1 - num2
+print(f"Result: {result}")
+result = num1 * num2
+print(f"Result: {result}")
+result = num1 / num2
+print(f"Result: {result}")
+result = num1 // num2
+print(f"Result: {result}")
+result = num1 % num2
+print(f"Result: {result}")
