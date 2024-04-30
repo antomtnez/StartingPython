5226a56b854b43c4f9d3576ab8ec07dd2f1e7416
Anto Martínez <antomartinezdev@gmail.com>
2024-04-30 12:52:16 +0200

    Working on variables and strings concatenation

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 myVar = "Hello world from Python"
 print(myVar)
 
-myVar = 2
-print(myVar)
-
-num1 = 10
-num2 = 2
-total = num1 + num2
+num1 = "10"
+num2 = "2"
+total = int(num1) + int(num2)
 print(total)
 
 print(id(num1))
 print(id(num2))
 print(id(total))
+
+print(type(myVar))
+print(type(num1))
+print(type(num2))
+print(type(total))
+
+favBand = "Metallica"
+comment = "Best Rock Band"
+
+print("My favourite band is", favBand, comment)
