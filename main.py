b10a33814ec5d32f3be3c99149b060388b059ce0
Anto Martínez <antomartinezdev@gmail.com>
2024-04-30 14:52:08 +0200

    Calculate if you are within a range of numbers

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-number = int(input("Introduce un edad para ver si eres mayor de edad: "))
-if number > 17:
-    print("Eres mayor de edad")
+number = int(input("Introduce un edad: "))
+overTwenty = number > 19
+underThirty = number <= 30
+if overTwenty and underThirty:
+    print("Estas entre los 20's y los 30's")
 else:
-    print("Eres menor de edad")
+    print("No estas entre los 20's y los 30's")
